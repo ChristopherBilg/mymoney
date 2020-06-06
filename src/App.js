6ab71cc961a892249e9ac7c2b146152e4bd80aa4
Christopher Bilger <christopherbilg@gmail.com>
2020-06-06 07:43:35 -0400

    Added webpack and babel support
    These are for bundling, and transpiling from es6 to browser es5 respectively.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Button from './components/Button/Button';
 
 const App = () => (
-  <h1>Hello world!</h1>
+  <>
+    <h1>Hello world!</h1>
+    <Button />
+  </>
 );
 
 export default App;
