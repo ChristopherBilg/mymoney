4a4fa16ae9128d5b8e37007f5b113fea61d83f47
Christopher Bilger <christopherbilg@gmail.com>
2020-06-05 22:08:27 -0400

    Added unit testing

---
+++ src/components/Button/Button.test.js
@@ -0,0 +1,9 @@
+const mocha = require('mocha');
+
+const assert = require('assert');
+
+mocha.describe('button test', () => {
+  mocha.it('should do this', () => {
+    assert.equal(true, true);
+  });
+});
