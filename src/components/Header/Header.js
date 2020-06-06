0cd90c1b788601a9469fd4e37442a7008ec8f64a
Christopher Bilger <christopherbilg@gmail.com>
2020-06-06 09:02:15 -0400

    Started work with blueprint, can't get styling to work however

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import {
+  Navbar, Alignment, NavbarHeading, NavbarGroup, Colors, NavbarDivider, Button,
+} from '@blueprintjs/core';
 
 const Header = () => (
-  <></>
+  <Navbar color={Colors.BLUE3} fixedToTop>
+    <NavbarGroup align={Alignment.LEFT}>
+      <NavbarHeading>mymoney</NavbarHeading>
+      <NavbarDivider />
+      <Button intent='success' text='Click me!' />
+    </NavbarGroup>
+  </Navbar>
 );
 
 export default Header;
